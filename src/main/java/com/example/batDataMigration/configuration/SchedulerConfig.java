519fdc8b52037424b63c2fc38b6f7dc1234f99ad
junyan li <jiangzhonping@163.com>
2022-03-06 20:14:05 -0500

    调整架构,不在使用batch,直接使用计划任务来实现,读取转换写入一次完成.
    此外,实现字段不完全匹配转换功能

--- src/main/java/com/example/batDataMigration/configuration/SchedulerConfig.java
+++ src/main/java/com/example/batDataMigration/configuration/SchedulerConfig.java
@@ -2,7 +2,14 @@ package com.example.batDataMigration.configuration;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.List;
 
+import com.example.batDataMigration.primary.Employee;
+import com.example.batDataMigration.primary.EmployeeRepository;
+import com.example.batDataMigration.secondary.Manager;
+import com.example.batDataMigration.secondary.ManagerRepository;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeMap;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
@@ -16,23 +23,25 @@ import org.springframework.scheduling.annotation.Scheduled;
 @Configuration
 @EnableScheduling
 public class SchedulerConfig {
-
     @Autowired
-    JobLauncher jobLauncher;
-
+    private EmployeeRepository employeeRepository;
     @Autowired
-    Job job;
+    private ManagerRepository managerRepository;
 
-    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S");
 
-//    @Scheduled(fixedDelay = 50000, initialDelay = 5000)
-    @Scheduled(cron = "0 */10 * * * *")  //Every 10 minutes
+
+    @Scheduled(fixedDelay = 24*3600*1000, initialDelay = 5000)
+//    @Scheduled(cron = "0 */10 * * * *")  //Every 10 minutes
     public void scheduleByFixedRate() throws Exception {
         System.out.println("Batch job starting");
-        JobParameters jobParameters = new JobParametersBuilder()
-                .addString("time", format.format(Calendar.getInstance().getTime()))
-                .toJobParameters();
-        jobLauncher.run(job, jobParameters);
+        List<Employee> employeeList = employeeRepository.findAll();
+        for(Employee employee : employeeList) {
+            ModelMapper modelMapper = new ModelMapper();
+            TypeMap<Employee, Manager> propertyMapper = modelMapper.createTypeMap(Employee.class,Manager.class);
+            propertyMapper.addMappings(mapper -> mapper.skip(Manager::setManagerId));
+            Manager manager = modelMapper.map(employee, Manager.class);
+            managerRepository.save(manager);
+        }
         System.out.println("********Batch job executed successfully*************\n");
     }
 }
--- src/main/java/com/example/batDataMigration/secondary/Manager.java
+++ src/main/java/com/example/batDataMigration/secondary/Manager.java
@@ -10,9 +10,9 @@ import javax.persistence.Id;
 @Entity
 @Data
 public class Manager {
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
-    private Long id;
+    private Long managerId;
     private String name;
     private int salary;
 }
